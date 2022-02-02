62e4c258bee4ec383a7521c2e517876802c1f3ff
nicomorillo <nicolasm.morillomartinez@gmail.com>
2022-02-02 23:52:01 +0000

    Processed len = 1

--- encription.py
+++ encription.py
@@ -1,13 +1,30 @@
 str = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj. "
-z = str.split()
-abc = {"a": "c",
-"b":"d", "c":"e", "d":"f", "e":"g", "f":"h", "g":"i", "h":"j", "i":"k", "j":"k", "l":"n", "m":"o", "n":"p", "o":"q", "p":"r", "q":"s", "r":"t", "s":"u", "t":"v", "u":"w", "v":"x", "w":"y", "x":"z", "y":"a", "z":"b"}
-def descr(q,p):
-  h=[]
-  for x in q:
-    if x.isalpha():
-      h+=p[x]
-    else:
-      h+=x
-  print("".join(h))
-descr(z,abc)
+str_list = str.split()
+abc = {"a":"c", "b":"d", "c":"e", "d":"f", "e":"g", "f":"h", "g":"i", "h":"j", "i":"k", "j":"k", "k":"m", "l":"n", "m":"o", "n":"p", "o":"q", "p":"r", "q":"s", "r":"t", "s":"u", "t":"v", "u":"w", "v":"x", "w":"y", "x":"z", "y":"a", "z":"b"}
+
+
+str1=["agh", "fg"]
+
+def descf(lista,cod):
+    nw_list=[]
+    nw2_list = ""
+    for ln in lista:
+        z = len(ln)
+        if z == 1:
+            nw_list += cod[ln]
+        else:
+            for x in ln:
+                nw2_list += cod[x]
+    print(nw_list)
+
+descf(str1,abc)
+
+#def descr(q,p):
+ # h=[]
+  #for x in q:
+   # if x.isalpha():
+    #  h+=p[x]
+    #else:
+     # h+=x
+  #print(" ".join(h))
+#descr(z,abc)
