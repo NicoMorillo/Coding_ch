55aa90e179081eb49ad9a7758339fad59d22f192
nicomorillo <nicolasm.morillomartinez@gmail.com>
2022-02-06 21:13:41 +0000

    Problem solved, and add txt

--- Find_caracter.py
+++ Find_caracter.py
@@ -1,10 +1,17 @@
 
 stra = "abcdddffffffffd"
+strm = open ('Level2.txt')
+strma = strm.read()
 
-asd = {}
+def check(mss):
+    asd = {}
+    for x in mss:
+        total = mss.count(x)
+        asd[x]= asd.get(x,0 ) +1
+    return(asd)
 
-for x in stra:
-    total = stra.count(x)
-    asd[x]= asd.get(x,0) + 1
+a = check(strma)
 
-print(asd)
+for z,c in a.items():
+    if c == 1:
+        print(z)
